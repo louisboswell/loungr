e5e19f90b5b55385096ca7472f18ece1300bddbf
louisboswell <ed193lb@leeds.ac.uk>
2021-12-07 17:33:59 +0000

    Edit profile and changed the title structure.

--- app/models.py
+++ app/models.py
@@ -10,6 +10,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -20,6 +22,7 @@ class User(UserMixin, db.Model):
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,15 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from flask_login import current_user, login_user, login_required, logout_user
 from app.models import User
 from werkzeug.urls import url_parse
+from datetime import datetime
 
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
-
     posts = [
         { 'author': {'username': 'Jack'},
             'body': "What's black and white and read all over?"
@@ -18,7 +18,7 @@ def index():
             'body': "Penguin in a blender"
         }
     ]
-    return render_template('index.html', posts=posts)
+    return render_template('index.html', posts=posts, title = 'Humour Hub')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -41,7 +41,7 @@ def login():
 
         return redirect(next_page)
 
-    return render_template('login.html', form=form)
+    return render_template('login.html', form=form, title='Humour Hub - Login')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -57,7 +57,7 @@ def register():
         db.session.commit()
 
         return redirect(url_for('login'))
-    return render_template('register.html', form=form)
+    return render_template('register.html', form=form, title = 'Humour Hub - Register')
 
 @app.route('/user/<username>')
 @login_required
@@ -67,7 +67,31 @@ def user(username):
         {'author': user, 'body': 'Test post #1'},
         {'author': user, 'body': 'Test post #2'}
     ]
-    return render_template('user.html', user=user, posts=posts)
+    return render_template('user.html', user=user, posts=posts, title = user.username)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your profile has been updated.')
+        return redirect(url_for('edit_profile'))
+    
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    
+    return render_template('edit_profile.html', form=form, title = 'Edit Profile')
 
 @app.route('/logout')
 def logout():
