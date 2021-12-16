0921c992d4a987f9598c1d1fb9eeb78d07468323
Louis Boswell <ed193lb@leeds.ac.uk>
2021-12-16 22:21:07 +0000

    Logging and advanced feature working. Ready for report.

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_admin import Admin
+import logging
 
 
+logging.basicConfig(filename='record.log', level=logging.DEBUG, format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
 app = Flask(__name__)
 admin = Admin(app, template_mode='bootstrap3')
--- app/routes.py
+++ app/routes.py
@@ -68,12 +68,14 @@ def login():
             return redirect(url_for('login'))
         
         login_user(user, remember=form.remember_me.data)
+        app.logger.info("User %s logged in", user.username)
 
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
 
         return redirect(next_page)
+
 
     return render_template('login.html', form=form, title='Login')
 
@@ -94,6 +96,7 @@ def register():
             user.followed.remove(x)
 
         db.session.commit()
+        app.logger.info("User %s registered in", user.username)
 
         return redirect(url_for('login'))
     return render_template('register.html', form=form, title = 'Register')
@@ -133,6 +136,7 @@ def room(id):
     room = Room.query.filter_by(id=id).first_or_404()
     members = room.get_members()
 
+    app.logger.info("User %s viewed room id:%s successfully.", current_user.username, id)
 
     return render_template('room.html', room=room, members=members, user=current_user, title=room.name)
 
@@ -142,6 +146,7 @@ def deleteroom(id):
     room = Room.query.filter_by(id=id).first()
 
     if room is None:
+        app.logger.warning("User %s tried to delete room id:%s which doesn't exist.", current_user.username, id)
         return redirect(url_for('rooms'))
 
     form = EmptyForm()
@@ -152,6 +157,8 @@ def deleteroom(id):
     if form.validate_on_submit:
         db.session.delete(room)
         db.session.commit()
+        app.logger.info("User %s deleted room id:%s.", current_user.username, id)
+
 
 
     return render_template('deleteroom.html', room=room, form=form, title='Delete room?')
@@ -172,6 +179,8 @@ def edit_profile():
         current_user.about_me = form.about_me.data
         db.session.commit()
         flash('Your profile has been updated.')
+        app.logger.info("User %s edited profile.", current_user.username)
+
         return redirect(url_for('edit_profile'))
     
     elif request.method == 'GET':
@@ -194,6 +203,9 @@ def changepassword():
         if form.new_password_1.data == form.new_password_2.data:
             user.set_password(form.new_password_1.data)
             db.session.commit()
+            app.logger.info("User %s changed password.", current_user.username)
+
+            
             return redirect(url_for('edit_profile'))
 
             
@@ -203,7 +215,10 @@ def changepassword():
 
 @app.route('/logout')
 def logout():
+    app.logger.info("User %s logged out.", current_user.username)
+
     logout_user()
+
     return redirect(url_for('index'))
 
 @app.route('/follow/<username>', methods=['POST'])
@@ -212,15 +227,21 @@ def follow(username):
     form = EmptyForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=username).first()
+
         if user is None:
             flash('User {} not found.'.format(username))
+            app.logger.warning("User %s failed to followed user %s.", current_user.username, username)
             return redirect(url_for('index'))
+
         if user == current_user:
             flash('You cannot follow yourself!')
             return redirect(url_for('user', username=username))
+
         current_user.follow(user)
         db.session.commit()
         flash('You are following {}!'.format(username))
+        app.logger.info("User %s followed user %s.", current_user.username, username)
+
         return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
@@ -229,16 +250,24 @@ def follow(username):
 @login_required
 def unfollow(username):
     form = EmptyForm()
+    
     if form.validate_on_submit():
         user = User.query.filter_by(username=username).first()
+
         if user is None:
             flash('User {} not found.'.format(username))
             return redirect(url_for('index'))
+            app.logger.warning("User %s failed to unfollow user %s.", current_user.username, username)
+
+
         if user == current_user:
             flash('You cannot unfollow yourself!')
             return redirect(url_for('user', username=username))
         current_user.unfollow(user)
         db.session.commit()
+        app.logger.info("User %s  unfollowed user %s.", current_user.username, username)
+
+
         flash('You are not following {}.'.format(username))
         return redirect(url_for('user', username=username))
     else:
@@ -262,6 +291,9 @@ def createroom():
         temp.set_name(form.name.data)
         temp.set_desc(form.desc.data)
         db.session.commit()
+
+        app.logger.info("User %s created room id %s.", current_user.username, temp.id)
+
         flash('{} has been created.'.format(form.name.data))
         return redirect(url_for('rooms'))
 
@@ -273,6 +305,10 @@ def join(id):
     room = Room.query.filter_by(id = id).first()
     current_user.join_room(room)
     db.session.commit()
+
+    app.logger.info("User %s joined room id %s.", current_user.username, id)
+
+
     return redirect(url_for('rooms'))
 
 @app.route('/leave/<id>', methods=['GET', 'POST'])
@@ -281,6 +317,9 @@ def leave(id):
     room = Room.query.filter_by(id = id).first()
     current_user.leave_room(room)
     db.session.commit()
+
+    app.logger.info("User %s left room id %s.", current_user.username, id)
+
     return redirect(url_for('rooms'))
 
 @app.route('/rooms/all', methods=['GET', 'POST'])
@@ -297,12 +336,19 @@ def allrooms():
 @login_required
 def like_action(post_id, action):
     post = Post.query.filter_by(id=post_id).first_or_404()
+
     if action == 'like':
         current_user.like_post(post)
         db.session.commit()
+        app.logger.info("User %s disliked post id %s.", current_user.username, post_id)
+
+
     if action == 'unlike':
         current_user.unlike_post(post)
         db.session.commit()
+        app.logger.info("User %s liked post id %s.", current_user.username, post_id)
+
+
     return redirect(request.referrer)
 
 @app.route('/report/<int:post_id>', methods=['GET', 'POST'])
@@ -312,6 +358,8 @@ def report_action(post_id):
     form = ReportForm()
 
     if form.validate_on_submit():
+        app.logger.info("User %s reported post id %s, with report body: %s", current_user.username, post_id, form.report_desc.data)
+
         return redirect(url_for('index'))
 
     return render_template('report.html', post=post, form=form, title="Report Post")
